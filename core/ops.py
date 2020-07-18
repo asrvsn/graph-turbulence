6e24c715290ec8e6ce47e66f94b1a467759a986c
Anand <anand@alphasheets.com>
2020-07-17 21:47:34 -0400

    cleanup

--- core/ops.py
+++
@@ -1,9 +0,0 @@
-''' Differential operators & forms on graphs '''
-
-from typing import Callable
-import numpy as np
-import networkx as nx
-
-from core.obs import *
-
-
--- examples/heat.py
+++ examples/heat.py
@@ -1,4 +1,4 @@
-''' Example usages of GraphDiffEq ''' 
+''' Heat equation on graphs ''' 
 
 import networkx as nx
 import numpy as np
@@ -8,53 +8,35 @@ import pdb
 from core.gde import *
 from rendering import *
 
-''' Heat equation '''
 n = 10
 G = nx.grid_2d_graph(n, n)
 
+# Boundaries
+upper, lower, left, right = [(0,j) for j in range(n)], [(n-1,j) for j in range(n)], [(i,0) for i in range(n)], [(i,n-1) for i in range(n)]
 
-def gde1():
-	# Initial conditions
-	v0 = np.zeros(len(G.nodes()))
-	l0 = np.zeros(len(G.edges()))
+def sys1():
+	alpha = 1.0
+	temp = VertexObservable(G, desc='Temperature')
+	temp.set_ode(lambda t: alpha*temp.laplacian())
+	temp.set_initial(y0=0.0)
+	temp.set_boundary(
+		dirichlet_values=dict(zip(upper + lower + left + right, [0.]*n + [0.5]*n + [1.]*n + [0.]*n))
+	)
 
-	# Difference equations
-	L = nx.laplacian_matrix(G)
-	dv_dt = lambda t, v, l: -v@L.T
-	dl_dt = lambda t, v, l: l0
+	sys = System([temp], desc=f'Heat equation (alpha={alpha}) with non-uniform Dirichlet boundary conditions')
+	return sys
 
-	# Set up problem
-	gde = GraphDiffEq(G, v0, l0, dv_dt, dl_dt, desc='Heat equation with non-uniform Dirichlet boundary conditions')
+def sys2():
+	alpha = 1.0
+	temp = VertexObservable(G, desc='Temperature')
+	temp.set_ode(lambda t: alpha*temp.laplacian())
+	temp.set_initial(y0=1.0)
+	temp.set_boundary(
+		dirichlet_values=dict(zip(upper + lower + left, repeat(1.0))), 
+		neumann_values=dict(zip(right[1:-1], repeat(-0.1)))
+	)
 
-	# Boundary conditions
-	dbound = [(i,0) for i in range(n)] + [(i,n-1) for i in range(n)] + [(0,j) for j in range(n)] + [(n-1,j) for j in range(n)] 
-	dvals = [0 for _ in range(n)] + [0.5 for _ in range(n)] + [1. for _ in range(n)] + [0 for _ in range(n)]
-	gde.set_vertex_boundary(dirichlet=dict(zip(dbound, dvals)))
+	sys = System([temp], desc=f'Heat equation (alpha={alpha}) with mixed boundary conditions')
+	return sys
 
-	return gde
-
-def gde2():
-	# Initial conditions
-	v0 = np.ones(len(G.nodes()))
-	l0 = np.zeros(len(G.edges()))
-
-	# Difference equations
-	L = nx.laplacian_matrix(G)
-	dv_dt = lambda t, v, l: -v@L.T
-	dl_dt = lambda t, v, l: l0
-
-	# Set up problem
-	gde = GraphDiffEq(G, v0, l0, dv_dt, dl_dt, desc='Heat equation with mixed boundary conditions')
-
-	# Boundary conditions
-	dbound = [(i,0) for i in range(n)] + [(i,n-1) for i in range(n)] + [(0,j) for j in range(n)] 
-	dvals = repeat(1.0)
-	nbound = [(n-1,j) for j in range(1,n-1)] 
-	nvals = repeat(-0.1)
-	gde.set_vertex_boundary(dirichlet=dict(zip(dbound, dvals)), neumann=dict(zip(nbound, nvals)))
-
-	return gde
-
-
-# Render live
-render_live([gde1(), gde2()])
+render_live([sys1(), sys2()])
